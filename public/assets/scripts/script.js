498fef1ffc9bdbea648375bf8fd74e869a9eca10
zuraizzafar <zuraiz49@gmail.com>
2021-02-21 14:49:16 +0500

    added question 5

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -1,19 +1,19 @@
 function question1() {
     var cc = document.getElementById("question1");
     ccNum = cc.value;
-    if (ccNum.length==16) {
+    if (ccNum.length == 16) {
         lastDigits = ccNum.substring(12);
         encCC = "**** **** **** " + lastDigits;
         document.getElementById("question1-answer").innerText = encCC;
     }
-    else 
+    else
         alert("Enter Credit Card in supported format! (16 Digits)");
 }
 function question2() {
     var input = document.getElementById("question2");
     str = input.value;
     var i = 0;
-    if(str.includes("potato")) {
+    if (str.includes("potato")) {
         //Sir, I have used regular expressions in here.
         potatoArray = str.match(/potato/g);
         i = potatoArray.length;
@@ -28,7 +28,7 @@ function question3() {
     var str = input.value;
     var length = str.length;
     let answer = "";
-    for (let i = 0; i < length; i++){
+    for (let i = 0; i < length; i++) {
         answer += str[i] + str[i];
     }
     document.getElementById("question3-answer").innerText = answer;
@@ -40,14 +40,37 @@ function question4() {
     if (salary == "") {
         alert("Enter Salary!");
     }
-    if (salary <= 10000) {
-        gross = salary + (salary * (20 + 80)/100);
+    else {
+        if (salary <= 10000) {
+            gross = salary + (salary * (20 + 80) / 100);
+        }
+        else if (salary <= 20000) {
+            gross = salary + (salary * (25 + 90) / 100);
+        }
+        else if (salary > 20000) {
+            gross = salary + (salary * (30 + 95) / 100);
+        }
+        document.getElementById("question4-answer").innerText = gross;
     }
-    else if (salary <= 20000) {
-        gross = salary + (salary * (25 + 90)/100);
+}
+function question5() {
+    var input = document.getElementById("question5");
+    var units = parseInt(input.value);
+    if (units == 0) {
+        alert("Units cant be 0!")
     }
-    else if (salary > 20000) {
-        gross = salary + (salary * (30 + 95)/100);
+    else {
+        if (units > 0 && units <= 50) {
+            cost = 0.5;
+        } else if (units > 50 && units <= 150) {
+            cost = 0.75;
+        } else if (units > 150 && units <= 250) {
+            cost = 1.2;
+        } else {
+            cost = 1.5;
+        }
+        var bill = cost * units;
+        var netBill = bill + ((bill * 20)/100);
+        document.getElementById("question5-answer").innerText = netBill;
     }
-    document.getElementById("question4-answer").innerText = gross;
 }
